f84e62a7f4abcbaab12ff2d78cb7dcd12e4b038d
Ian Rash <redcodefinal@gmail.com>
2024-06-10 01:33:32 -0700

    Gonnan completely rewrite

--- src/crixel/collision/leaf.cr
+++ src/crixel/collision/leaf.cr
@@ -5,20 +5,19 @@ class Crixel::Quad::Leaf
   alias ID = UInt32 # TODO: Can change to UInt16?
   
   property? divided = false
+
   property nw : Leaf?
   property ne : Leaf?
   property sw : Leaf?
   property se : Leaf?
 
-  getter id : ID 
-
-  getter parent : Leaf? = nil
 
   getter current_depth = 0
 
-  def initialize(parent : Leaf, @id, @x, @y, @width, @height)
-    @parent = parent
-    @current_depth = parent.current_depth + 1
+  getter children = Set(Crixel::ID).new
+
+  def initialize(depth, @x, @y, @width, @height)
+    @current_depth = depth
   end
 
   def nw!
@@ -37,8 +36,28 @@ class Crixel::Quad::Leaf
     @se.not_nil!
   end
 
-  def root?
-    parent.nil?
+  def merge
+    if divided?
+      nw!.merge
+      ne!.merge
+      sw!.merge
+      se!.merge
+
+      @children.concat nw!.children
+      @children.concat ne!.children
+      @children.concat sw!.children
+      @children.concat se!.children
+    end
+  end
+
+  def subdivide
+    @divided = true
+    w = width/2
+    h = height/2
+    @nw = Leaf.new(current_depth+1, x, y, w, h)
+    @ne = Leaf.new(current_depth+1, x + w, y, w, h)
+    @sw = Leaf.new(current_depth+1, x, y + h, w, h)
+    @se = Leaf.new(current_depth+1, x + w, y + h, w, h)
   end
 
   # Returns what leaf this object should insert into
@@ -57,43 +76,14 @@ class Crixel::Quad::Leaf
     return self
   end
 
-  def get_intersecting(bounds : IBody) : Set(Leaf)
-    output = Set(Leaf).new
-    if bounds.intersects?(self)
-      output << self
-      
-      if self.divided?
-        output.concat nw!.get_intersecting(bounds)
-        output.concat ne!.get_intersecting(bounds)
-        output.concat sw!.get_intersecting(bounds)
-        output.concat se!.get_intersecting(bounds)
-      end
-    end
-    output
+  def leafs : StaticArray(Leaf, 4)
+    return Leaf[nw!, ne!, sw!, se!] if divided?
+    raise("leafs called but not divided :(")
   end
 
-  def get_lineage : Set(Leaf)
-    output = Set(Leaf).new
-    output << self
-    if p = parent
-      output.concat p.get_lineage
-    end
-    output
-  end
-
-
-  def get_root : Leaf
-    parent = @parent
-
-    until parent.root?
-      parent = parent.parent
-    end
-
-    parent
-  end
-
-  def equals?(other : self)
-    self.id == other.id
+  def leafs? : StaticArray(Leaf, 4)?
+    return Leaf[nw!, ne!, sw!, se!] if divided?
+    return nil
   end
 
   def draw(color : Color = Color::RED)
--- src/crixel/collision/quadtree.cr
+++ src/crixel/collision/quadtree.cr
@@ -1,9 +1,14 @@
 require "./leaf"
 class Crixel::Quad::Tree < Crixel::Quad::Leaf
+  class Item
+    property object : Basic
+    property leaf : Leaf
+
+    def initialize(@basic, @leaf)
+  end
+
   # Hash of objects to the leaf it's inside
-  @objects = {} of Crixel::ID => NamedTuple(item: Basic, leaf: Leaf)
-  # Hash of leaf nodes to the list of objects contained inside it
-  @leafs = {} of Leaf::ID => NamedTuple(leaf: Leaf, items: Set(Basic))
+  @items = {} of Crixel::ID => Item
 
   property max_depth = 5
   property max_children = 5
@@ -13,72 +18,23 @@ class Crixel::Quad::Tree < Crixel::Quad::Leaf
     @id = 0
   end
 
-  def total_leafs
-    @leafs.size
-  end
-
-  def total_objects
-    @objects.size
+  def total_items
+    @items.size
   end
 
   def includes?(child : Basic)
-    !!@objects[child.id]?
+    !!@items[child.id]?
   end
 
   def includes?(id : Crixel::ID)
-    !!@objects[id]?
+    !!@items[id]?
   end
 
   def insert(child : Basic) : Leaf
     raise("Not insertable here (doesnt have a shape)") unless child.is_a?(IBody)
     raise("Not insertable here (is not Collidable)") unless child.is_a?(Collidable)
 
-    if old = @objects[child.id]?
-      # Remove from the leaf pool
-      _remove_from_leaf(old[:leaf], child)
-    end
-
-    current_leaf = _insert(child, self)
-
-    @objects[child.id] = {item: child, leaf: current_leaf}
-    return current_leaf
-  end
-
-  private def _insert(child : Basic, leaf : Leaf) : Leaf
-    current_leaf = leaf.get_container(child.as(IBody))
-
-    # Check if our child count would be over max children
-    if @leafs[current_leaf.id]? && (@leafs[current_leaf.id][:items].size + 1) > max_children
-      if _subdivide(current_leaf)
-        # Trickle old items down into new buckets
-        @leafs[current_leaf.id][:items].each do |item|
-          new_parent = current_leaf.get_container(item.as(IBody))
-          if new_parent != current_leaf
-            current_leaf = _insert(child, new_parent)
-          end
-        end
-      end
-    end
-
-    _add_to_leaf(current_leaf, child)
-    return current_leaf
-  end
-
-  # private def _move(child : Basic, from : Leaf, to : Leaf)
-  #   @objects[child.id] = {item: child, leaf: to}
-  #   _remove_from_leaf(from, child)
-  #   _add_to_leaf(to, child)
-  # end
-
-  def remove(child : Basic)
-    remove(child.id)
-  end
-
-  def remove(child_id : Crixel::ID)
-    if old = @objects[child_id]?
-      _remove_from_leaf(old[:leaf], old[:item])
-      @objects.delete(child_id)
-    end
+    
   end
 
   def search(bounds : IBody, &block : Proc(Basic, Nil))
@@ -89,57 +45,16 @@ class Crixel::Quad::Tree < Crixel::Quad::Leaf
     search(bounds.x, bounds.y, bounds.width, bounds.height)
   end
 
-  def search(x, y, w, h, &block : Proc(Basic, Nil))
-    search_area = Rectangle.new(x, y, w, h)
-    get_intersecting(search_area).each do |leaf|
-      @leafs[leaf.id][:items].each { |i| yield i }
-    end
-  end
-
   def search(x, y, w, h) : Array(Basic)
     output = [] of Basic
     search(x, y, w, h) {|item| output << item}
     output
   end
 
-  private struct CheckedPair
-    getter b1 : Crixel::ID
-    getter b2 : Crixel::ID
-
-    def initialize(@b1, @b2)
-    end
-
-    # def equals?(other : CheckedPair)
-    #   (b1 == other.b1 && b2 == other.b2) || 
-    #   (b2 == other.b1 && b1 == other.b2)
-    # end
-
-    def hash
-      b1.to_u64 + (b2.to_u64 >> 32) ^ b2.to_u64 + (b1.to_u64 >> 32)
-    end
+  def search(x, y, w, h, &block : Proc(Basic, Nil))
   end
 
   def check(&block : Proc(Basic, Basic, Nil))
-    @total_checks = 0
-    already_checked = Set(CheckedPair).new
-
-    @objects.each do |c_id, object|
-      leafs = object[:leaf].get_lineage
-
-      leafs.each do |leaf|
-        @leafs[leaf.id][:items].each do |other|
-          next if other == object[:item] 
-          pair = CheckedPair.new(other.id, object[:item].id)
-          next if already_checked.includes?(pair)
-          @total_checks += 1
-
-          already_checked << pair
-          b1 = other.as(IBody)
-          b2 = object[:item].as(IBody)
-          yield other, object[:item] if b1.intersects?(b2)
-        end
-      end
-    end
   end
 
   def draw_grid(no_items_color : Color = Color::RED, items_color : Color = Color::BLUE)
@@ -148,51 +63,5 @@ class Crixel::Quad::Tree < Crixel::Quad::Leaf
     @leafs.each do |id, leaf_info|
       leaf_info[:leaf].draw_body(items_color)
     end
-  end
-
-
-  private def _add_to_leaf(leaf : Leaf, child : Basic)
-    @leafs[leaf.id] = {leaf: leaf, items: Set(Basic).new} unless @leafs[leaf.id]?
-    @leafs[leaf.id][:items] << child
-  end
-  
-  private def _remove_from_leaf(leaf : Leaf, child : Basic)
-    _remove_from_leaf(leaf.id, child.id)
-  end
-
-  private def _remove_from_leaf(leaf : Leaf, child_id : Crixel::ID)
-    _remove_from_leaf(leaf.id, child_id)
-  end
-  
-  private def _remove_from_leaf(leaf_id : Leaf::ID, child : Basic)
-    _remove_from_leaf(leaf_id, child.id)
-  end
-
-  private def _remove_from_leaf(leaf_id : Leaf::ID, child_id : Crixel::ID)
-    @leafs[leaf_id][:items].delete(child_id)
-
-    if @leafs[leaf_id][:items].empty?
-      puts "Deleting leaf #{leaf_id}"
-      @leafs.delete leaf_id
-    end
-  end
-
-  private def _subdivide(leaf : Leaf) : Bool
-    return false if leaf.current_depth >= max_depth
-    leaf.divided = true
-    w = leaf.width/2
-    h = leaf.height/2
-    leaf.nw = Leaf.new(leaf, get_id, x, y, w, h)
-    leaf.ne = Leaf.new(leaf, get_id, x + w, y, w, h)
-    leaf.sw = Leaf.new(leaf, get_id, x, y + h, w, h)
-    leaf.se = Leaf.new(leaf, get_id, x + w, y + h, w, h)
-    return true
-  end
-
-  @current_id = 1_u32
-  private def get_id
-    old = @current_id
-    @current_id += 1
-    return old
   end
 end
